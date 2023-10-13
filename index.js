eb2023d5258e6790da448a087fb84fd6cdce6841
Mutahir <mutahir.khan44@yahoo.com>
2023-10-13 17:29:55 +0500

    basic route setup to get

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
+app.get('/', (req, res) => {
+    res.send("Hello users. This is my API")
+})
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
